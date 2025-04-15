c65dcd304c6cec69e2fd8102c8dbfc0dbac089e9
wjdaudgy1994@ex-it.co.kr <wjdaudgy1994@ex-it.co.kr>
2025-04-15 16:29:42 +0900

    commit

--- resources/assets/js/daumPstUtil.js
+++ resources/assets/js/daumPstUtil.js
@@ -5,11 +5,22 @@ https://postcode.map.daum.net/guide
 <script type="text/javascript" src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
 */
 const daumPstUtil = {
-	fn_get : function(id, fn) {
+	fn_get : function() {
+		var args	= (daumPstUtil.fn_get.arguments);
+		var len		= (args.length);
+		var id		= (args[0]);
+		var fn		= (args[1]);
+		var h		= ((len > 2) ? (args[2]) : (window.innerHeight));
+		var w		= ((len > 3) ? (args[3]) : (window.innerWidth));
+		var z		= ((len > 4) ? (args[4]) : ("0"));
+		$("#" + (id)).empty();
+		$("#" + (id)).css("height", h);
+		$("#" + (id)).css("width", w);
+		$("#" + (id)).css("z-index", z);
 		var html = (document.getElementById(id));
 		daum.postcode.load(function() {
 			(new daum.Postcode({
-				oncomplete			: function() {fn($(this));},
+				oncomplete			: function(rst) {fn(rst);},
 				width				: ("100%"),
 				height				: ("100%"),
 				focusInput			: (false),
@@ -24,7 +35,5 @@ const daumPstUtil = {
 			}).embed(html));
 			html.style.display		= ("block");
 		});
-		$("#" + (id)).css("height", window.innerHeight);
-		$("#" + (id)).css("width", window.innerWidth);
 	}
 };
--- resources/assets/js/kakaoMapUtil.js
+++ resources/assets/js/kakaoMapUtil.js
@@ -9,13 +9,17 @@ let kakaoMapUtil = {
 	_map				: (null),
 	_min_lv				: (3),
 	_max_lv				: (15),
-	_clstr				: (null),
-	_clstr_lv			: (8),
 	_cntr_lat			: ("37.5565789226072"),
 	_cntr_lot			: ("126.959278026157"),
 	_pst				: ([]),
 	_mrk				: ([]),
-	fn_get				: function(id) {
+	fn_get				: function() {
+		var args	= (kakaoMapUtil.fn_get.arguments);
+		var len		= (args.length);
+		var id		= (args[0]);
+		var h		= ((len > 1) ? (args[1]) : (window.innerHeight));
+		var w		= ((len > 2) ? (args[2]) : (window.innerWidth));
+		var z		= ((len > 3) ? (args[3]) : ("0"));
 		$("#" + (id)).empty();
 		var ops = {
 			center					: (kakaoMapUtil.fn_lat_lot(kakaoMapUtil._cntr_lat, kakaoMapUtil._cntr_lot)),
@@ -29,20 +33,12 @@ let kakaoMapUtil = {
 			keyboardShortcuts		: (false)
 		};
 		kakaoMapUtil._map = (new kakao.maps.Map(document.getElementById(id), ops));
-		setTimeout(() => kakaoMapUtil.fn_resz(id), 500);
-		setTimeout(() => kakaoMapUtil.fn_hndlr(), 600);
-		setTimeout(() => kakaoMapUtil.fn_clstr(), 700);
-		setTimeout(() => kakaoMapUtil.fn_gps(), 800);
+		setTimeout(() => kakaoMapUtil.fn_resz(id, h, w, z), 500);
+		setTimeout(() => kakaoMapUtil._map.relayout(), 600);
+		setTimeout(() => kakaoMapUtil.fn_hndlr(), 700);
+		setTimeout(() => kakaoMapUtil.fn_gps(kakaoMapUtil.fn_gps_y, kakaoMapUtil.fn_gps_n), 800);
 	},
-	fn_clstr			: function() {
-		var ops = {
-			map				: (kakaoMapUtil._map),
-			averageCenter	: (true),
-			minLevel		: (kakaoMapUtil._clstr_lv)
-		};
-		kakaoMapUtil._clstr = (new kakao.maps.MarkerClusterer(ops));
-	},
-	fn_gps				: function() {
+	fn_gps				: function(fn_y, fn_n) {
 		var f = ("geolocation" in navigator && navigator.geolocation);
 		if (f) {
 			var ops = {
@@ -50,9 +46,9 @@ let kakaoMapUtil = {
 				timeout					: (3000),
 				maximumAge				: (0)
 			};
-			navigator.geolocation.getCurrentPosition(kakaoMapUtil.fn_gps_y, kakaoMapUtil.fn_gps_n, ops);
+			navigator.geolocation.getCurrentPosition(fn_y, fn_n, ops);
 		} else {
-			kakaoMapUtil.fn_gps_n();
+			fn_n();
 		}
 	},
 	fn_gps_y			: function(position) {
@@ -67,51 +63,48 @@ let kakaoMapUtil = {
 	fn_data				: function() {
 		kakaoMapUtil.fn_prvnt(false);
 		$.ajax({
-				url				: "/json/csCpInfoPage/csGroupList?groupBy=sid",
-				data			: {},
-				type			: "get",
-				dataType		: "json",
-				contentType		: "application/json; charset=UTF-8",
-				success:function(data) {
-					if (data != null && data.list != null && data.cnt != null && data.cnt > 0) {
-						$(data.list).each(function() {
-							kakaoMapUtil._pst.push({"latLot":(kakaoMapUtil.fn_lat_lot((this.gps.split(",")[0]), (this.gps.split(",")[1])))});
-						});
-					}
-					kakaoMapUtil.fn_del_mrk();
+			url				: "/json/csCpInfoPage/csGroupList",
+			data			: {},
+			type			: "get",
+			dataType		: "json",
+			contentType		: "application/json; charset=UTF-8",
+			success:function(data) {
+				if (data != null && data.list != null && data.cnt != null && data.cnt > 0) {
+					$(data.list).each(function() {
+						kakaoMapUtil._pst.push({"latLot":(kakaoMapUtil.fn_lat_lot((this.gps.split(",")[0]), (this.gps.split(",")[1])))});
+					});
 				}
-			});
+				kakaoMapUtil.fn_del_mrk();
+			}
+		});
 	},
 	fn_del_mrk			: function() {
 		$(kakaoMapUtil._mrk).each(function(i) {
 			kakaoMapUtil._mrk[i].setMap(null);
 		});
 		kakaoMapUtil._mrk = ([]);
-		kakaoMapUtil._clstr.clear();
 		kakaoMapUtil.fn_add_mrk();
 	},
 	fn_add_mrk			: function() {
 		$(kakaoMapUtil._pst).each(function() {
 			kakaoMapUtil._mrk.push(kakaoMapUtil.fn_mrk(this));
 		});
-		kakaoMapUtil._clstr.addMarkers(kakaoMapUtil._mrk);
 		kakaoMapUtil.fn_prvnt(true);
 	},
 	fn_mrk				: function(obj) {
-		var mrk = new kakao.maps.Marker({
+		var mrk = (new kakao.maps.Marker({
 			map			: (kakaoMapUtil._map),
 			position	: (obj.latLot)
-		});
+		}));
 		kakao.maps.event.addListener(mrk, "click", function() {
 			kakaoMapUtil.fn_move(kakaoMapUtil._min_lv, obj.latLot);
 		});
 		return (mrk);
 	},
-	fn_resz				: function(id) {
-		$("#" + (id)).css("height", window.innerHeight);
-		$("#" + (id)).css("width", window.innerWidth);
-		$("#" + (id)).css("z-index", "0");
-		setTimeout(() => kakaoMapUtil._map.relayout(), 100);
+	fn_resz				: function(id, h, w, z) {
+		$("#" + (id)).css("height", h);
+		$("#" + (id)).css("width", w);
+		$("#" + (id)).css("z-index", z);
 	},
 	fn_lat_lot			: function(lat, lot) {
 		return (new kakao.maps.LatLng(((lat == null || lat == "" || lat == undefined) ? (kakaoMapUtil._cntr_lat) : (lat)), ((lot == null || lot == "" || lot == undefined) ? (kakaoMapUtil._cntr_lot) : (lot))));
@@ -131,14 +124,131 @@ let kakaoMapUtil = {
 		kakaoMapUtil._map.setKeyboardShortcuts(tp);
 	}
 };
+let kakaoClstrMapUtil = {
+	_map				: (null),
+	_min_lv				: (3),
+	_max_lv				: (15),
+	_clstr				: (null),
+	_clstr_lv			: (8),
+	_pst				: ([]),
+	_mrk				: ([]),
+	fn_get				: function(id) {
+		var args	= (kakaoClstrMapUtil.fn_get.arguments);
+		var len		= (args.length);
+		var id		= (args[0]);
+		var h		= ((len > 1) ? (args[1]) : (window.innerHeight));
+		var w		= ((len > 2) ? (args[2]) : (window.innerWidth));
+		var z		= ((len > 3) ? (args[3]) : ("0"));
+		$("#" + (id)).empty();
+		var ops = {
+			center					: (kakaoMapUtil.fn_lat_lot(kakaoMapUtil._cntr_lat, kakaoMapUtil._cntr_lot)),
+			level					: (kakaoClstrMapUtil._min_lv),
+			maxLevel				: (kakaoClstrMapUtil._max_lv),
+			draggable				: (false),
+			zoomable				: (false),
+			scrollwheel				: (false),
+			disableDoubleClick		: (true),
+			disableDoubleClickZoom	: (true),
+			keyboardShortcuts		: (false)
+		};
+		kakaoClstrMapUtil._map = (new kakao.maps.Map(document.getElementById(id), ops));
+		setTimeout(() => kakaoMapUtil.fn_resz(id, h, w, z), 500);
+		setTimeout(() => kakaoClstrMapUtil._map.relayout(), 600);
+		setTimeout(() => kakaoClstrMapUtil.fn_hndlr(), 700);
+		setTimeout(() => kakaoClstrMapUtil.fn_clstr(), 800);
+		setTimeout(() => kakaoMapUtil.fn_gps(kakaoClstrMapUtil.fn_gps_y, kakaoClstrMapUtil.fn_gps_n), 900);
+	},
+	fn_clstr			: function() {
+		var ops = {
+			map				: (kakaoClstrMapUtil._map),
+			averageCenter	: (true),
+			minLevel		: (kakaoClstrMapUtil._clstr_lv)
+		};
+		kakaoClstrMapUtil._clstr = (new kakao.maps.MarkerClusterer(ops));
+	},
+	fn_gps_y			: function(position) {
+		if (position != null) {
+			kakaoClstrMapUtil.fn_move(kakaoClstrMapUtil._min_lv, (kakaoMapUtil.fn_lat_lot(position.coords.latitude, position.coords.longitude)));
+		}
+		kakaoClstrMapUtil.fn_data();
+	},
+	fn_gps_n			: function() {
+		kakaoClstrMapUtil.fn_data();
+	},
+	fn_data				: function() {
+		kakaoClstrMapUtil.fn_prvnt(false);
+		$.ajax({
+			url				: "/json/csCpInfoPage/csGroupList",
+			data			: {},
+			type			: "get",
+			dataType		: "json",
+			contentType		: "application/json; charset=UTF-8",
+			success:function(data) {
+				if (data != null && data.list != null && data.cnt != null && data.cnt > 0) {
+					$(data.list).each(function() {
+						kakaoClstrMapUtil._pst.push({"latLot":(kakaoMapUtil.fn_lat_lot((this.gps.split(",")[0]), (this.gps.split(",")[1])))});
+					});
+				}
+				kakaoClstrMapUtil.fn_del_mrk();
+			}
+		});
+	},
+	fn_del_mrk			: function() {
+		$(kakaoClstrMapUtil._mrk).each(function(i) {
+			kakaoClstrMapUtil._mrk[i].setMap(null);
+		});
+		kakaoClstrMapUtil._mrk = ([]);
+		((kakaoClstrMapUtil._clstr != null) ? (kakaoClstrMapUtil._clstr.clear()) : ("")); 
+		kakaoClstrMapUtil.fn_add_mrk();
+	},
+	fn_add_mrk			: function() {
+		$(kakaoClstrMapUtil._pst).each(function() {
+			kakaoClstrMapUtil._mrk.push(kakaoClstrMapUtil.fn_mrk(this));
+		});
+		((kakaoClstrMapUtil._clstr != null) ? (kakaoClstrMapUtil._clstr.addMarkers(kakaoClstrMapUtil._mrk)) : (""));
+		kakaoClstrMapUtil.fn_prvnt(true);
+	},
+	fn_mrk				: function(obj) {
+		var mrk = (new kakao.maps.Marker({
+			map			: (kakaoClstrMapUtil._map),
+			position	: (obj.latLot)
+		}));
+		kakao.maps.event.addListener(mrk, "click", function() {
+			kakaoClstrMapUtil.fn_move(kakaoClstrMapUtil._min_lv, obj.latLot);
+		});
+		return (mrk);
+	},
+	fn_move				: function(lv, latLot) {
+		kakaoClstrMapUtil._map.jump(latLot, lv);
+	},
+	fn_hndlr			: function() {
+		kakao.maps.event.addListener(kakaoClstrMapUtil._map, "zoom_changed", function() {
+		});
+		kakao.maps.event.addListener(kakaoClstrMapUtil._map, "dragend", function() {
+		});
+	},
+	fn_prvnt			: function(tp) {
+		kakaoClstrMapUtil._map.setDraggable(tp);
+		kakaoClstrMapUtil._map.setZoomable(tp);
+		kakaoClstrMapUtil._map.setKeyboardShortcuts(tp);
+	}
+};
 let kakaoStaticMapUtil = {
 	_map				: (null),
 	_lv					: (3),
-	fn_get				: function(id, lat, lot) {
+	fn_get				: function() {
+		var args	= (kakaoStaticMapUtil.fn_get.arguments);
+		var len		= (args.length);
+		var id		= (args[0]);
+		var lat		= (args[1]);
+		var lot		= (args[2]);
+		var h		= ((len > 3) ? (args[3]) : (window.innerHeight));
+		var w		= ((len > 4) ? (args[4]) : (window.innerWidth));
+		var z		= ((len > 5) ? (args[5]) : ("0"));
 		$("#" + (id)).empty();
-		$("#" + (id)).css("height", window.innerHeight);
-		$("#" + (id)).css("width", window.innerHeight);
-		$("#" + (id)).css("z-index", "0");
+		$("#" + (id)).css("height", h);
+		$("#" + (id)).css("width", w);
+		$("#" + (id)).css("z-index", z);
 		var ops = {
 			center		: (new kakao.maps.LatLng(lat, lot)),
 			level		: (kakaoStaticMapUtil._lv),
@@ -147,5 +257,17 @@ let kakaoStaticMapUtil = {
 		kakaoStaticMapUtil._map = (new kakao.maps.StaticMap(document.getElementById(id), ops));
 		setTimeout(() => $("#" + (id)).find("a").attr("href", "javascript:void(0);"), 100);
 		setTimeout(() => $("#" + (id)).find("a").attr("target", ""), 200);
+	}
+};
+let kakaoGeocoderUtil = {
+	fn_get_addr			: function(lat, lot, fn) {
+		(new kakao.maps.services.Geocoder()).coord2RegionCode(lot, lat, function(rst, stat) {
+			(((rst == null || rst == "" || rst == undefined) || (stat == null || stat == "" || stat == undefined)) ? ("") : ((stat == kakao.maps.services.Status.OK && rst.length > 0) ? (fn(rst)) : ("")));
+		});
+	},
+	fn_get_gps			: function(addr, fn) {
+		(new kakao.maps.services.Geocoder()).addressSearch(addr, function(rst, stat) {
+			(((rst == null || rst == "" || rst == undefined) || (stat == null || stat == "" || stat == undefined)) ? ("") : ((stat == kakao.maps.services.Status.OK && rst.length == 1) ? (fn(rst)) : ("")));
+		});
 	}
 };
